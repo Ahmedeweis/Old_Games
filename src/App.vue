23e64fc4d2c9c61fb3f6b5f8863a32aff38c57c0
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2025-06-10 17:12:16 +0300

    .

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+     <h1 class=" bg-amber-400 text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <TestIcon />
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+<script setup>
+import TestIcon from './components/TestIcon.vue';
+</script>
+<style scoped>
+</style>
---
+++ src/components/TestIcon.vue
@@ -0,0 +1,11 @@
+  <template>
+  <div class="flex items-center space-x-4 bg-amber-100">
+    <i class="i-carbon-home w-6 h-6 " />
+    <i class="i-carbon-user w-6 h-6" />
+    <span>أيقونات مخصصة</span>
+  </div>
+  </template>
+  <script setup>
+import 'virtual:icons/carbon/home'; // استيراد أيقونة home
+import 'virtual:icons/carbon/user'; // استيراد أيقونة user
+</script>
