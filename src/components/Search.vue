5f7fbf9996994ad740cdbf178d543134ef068e56
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2025-07-02 15:26:51 +0300

    .

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex flex-col md:flex-row justify-center w-full bg-black items-center p-4 rounded-lg shadow-md gap-2">
-  <div class="flex flex-col md:flex-row items-center w-full max-w-2xl mx-auto gap-2">
+  <div class="flex flex-col md:flex-row items-center w-full max-w-2xl mx-auto gap-0.5">
     <!-- القائمة المنسدلة للتصنيفات -->
     <div class="relative w-full md:w-[120px]">
       <select class="appearance-none bg-gray-800 px-4 py-2 text-white rounded-lg md:rounded-l-lg md:rounded-r-none w-full focus:outline-none focus:ring-2 focus:ring-blue-500">
@@ -48,7 +48,7 @@
       </div>
     </div>
     <!-- زر البحث -->
-    <button class="text-white bg-gray-800 px-4 py-2 rounded-lg hover:bg-white transition duration-300 hover:text-gray-800 w-full md:w-auto">
+    <button class="text-white bg-gray-800 px-4 py-2 rounded-lg hover:bg-white transition duration-300 hover:text-gray-800 w-full md:w-auto ml-2">
       بحث
     </button>
   </div>
